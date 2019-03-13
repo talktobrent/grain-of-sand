4bcbc5f7eea7e0783f62a4e45200ba90843616c8
talktobrent <talktobrent@gmail.com>
2019-03-13 13:15:35 -0700

    Added autosuggest functionality, and changed return value to json

--- api/app.py
+++ api/app.py
@@ -1,55 +1,68 @@
 #!/usr/bin/python3
 """ api module """
 
-from flask import Flask
+from flask import Flask, jsonify
 import re
 import requests
-import urllib
 
 app = Flask(__name__)
 
 here = requests.Session()
 words = requests.Session()
-words.params = {"key": "VP9JUPVY", "display": "minimal"}
-here.params = {"app_id": "7DZj4FZSRWbx6FTL8JER", "app_code": "9SXulP-IrrM3ZJnMBwBHLQ"}
+words.params = {"key": "VP9JUPVY", "display": "terse"}
+here.params = {"app_id": "7DZj4FZSRWbx6FTL8JER",
+               "app_code": "9SXulP-IrrM3ZJnMBwBHLQ",
+               "maxresults": "1",
+               "language": "en"}
 
 @app.route('/api/<query>', methods=['GET'])
 def convert(query):
-    """ cleans query and converts """
-    old = query
-    query = urllib.parse.unquote(query)
+    """ tests query and converts """
     if re.match("^\w*\.\w*\.\w*$", query):
-        query = words.get('https://api.what3words.com/v2/forward?addr=' + query).json()
-        print(query)
+        string = query
+        query = words.get('https://api.what3words.com/v2/forward?addr=' + string).json()
         if query.get("status").get("code"):
-            return (old)
+            try:
+                query = words.get('https://api.what3words.com/v2/autosuggest?count=1&addr=' + string).json()
+                query = query.get("suggestions")[0]
+            except:
+                return ("bad")
+        w = query.get("words")
         coord = query.pop("geometry")
-        data = {"prox": "{},{},250".format(coord.get("lat"), coord.get("lng")),
+        data = {"prox": "{},{},500".format(coord.get("lat"), coord.get("lng")),
                 "mode": "retrieveAddresses",
                 "maxresults": 1}
-        print (data)
-        query = here.get('https://reverse.geocoder.api.here.com/6.2/reversegeocode.json', params=data).json()
-        print (query)
-
-        try:
-            address = query.pop("Response").pop("View")[0].pop("Result")[0].pop("Location").pop("Address").pop("Label")
-        except:
-            return (old)
-        return (address)
-
-    else:
-        query = here.get('https://geocoder.api.here.com/6.2/geocode.json?searchtext=' + old).json()
-        print(query)
+        query = here.get('https://reverse.geocoder.api.here.com/6.2/reversegeocode.json', params=data)
+        print(query.url)
+        query = query.json()
         try:
             query = query.pop("Response").pop("View")[0].pop("Result")[0].pop("Location")
-            coord = query.pop("NavigationPosition")[0]
+            coord = query.pop("DisplayPosition")
+            address = query.pop("Address").pop("Label")
         except:
-            return (old)
+            return ("bad")
+
+    else:
+        query = here.get('https://geocoder.api.here.com/6.2/search.json?searchtext=' + query)
+        print(query.url)
+        query = query.json()
+        try:
+            query = query.pop("Response").pop("View")[0].pop("Result")[0]
+            if query.get("MatchQuality").get("Name"):
+                address = query.get("Place").pop("Name")
+                query = query.get("Place").pop("Locations")[0]
+            else:
+                query = query.pop("Location")
+                address = query.pop("Address").pop("Label")
+            coord = query.pop("DisplayPosition")
+        except:
+            return ("bad")
         data = {"coords": "{},{}".format(coord.get("Latitude"), coord.get("Longitude"))}
-        print(data)
         query = words.get('https://api.what3words.com/v2/reverse', params=data).json()
-        print(query)
-        return (query.pop("words"))
+        w = query.get("words")
+
+    coord.update({"Address": address, "Words": w})
+    return jsonify(coord)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
