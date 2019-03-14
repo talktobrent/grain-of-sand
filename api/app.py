6aea33483336ac8fad16f87ac7c4449e84161d7b
talktobrent <talktobrent@gmail.com>
2019-03-14 15:30:15 -0700

    Added google oath route

--- api/app.py
+++ api/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ api module """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import re
 import requests
+from google.oauth2 import id_token
+from google.auth.transport import requests as grequests
 
 app = Flask(__name__)
 
@@ -68,6 +70,33 @@ def convert(query):
 
     coord.update({"Address": address, "Words": w})
     return jsonify(coord)
+
+@app.route('/api/verify', methods=['POST'])
+def verify():
+    token = request.get_json(silent=True)
+    try:
+        # Specify the CLIENT_ID of the app that accesses the backend:
+        idinfo = id_token.verify_oauth2_token(token, grequests.Request(), '550246531979-58rjevftor5s8knchrflgi2fevn04ud6')
+
+        # Or, if multiple clients access the backend server:
+        # idinfo = id_token.verify_oauth2_token(token, requests.Request())
+        # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
+        #     raise ValueError('Could not verify audience.')
+
+        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+            raise ValueError('Wrong issuer.')
+
+        # If auth request is from a G Suite domain:
+        # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
+        #     raise ValueError('Wrong hosted domain.')
+
+        # ID token is valid. Get the user's Google Account ID from the decoded token.
+        userid = idinfo['sub']
+        print(userid)
+        return (jsonify("OK"), 201)
+    except ValueError:
+        # Invalid token
+        pass
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
