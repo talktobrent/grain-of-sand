0403f1aa57b8fc49a573497cea17e2e028d3886c
dspham <dsvpham@gmail.com>
2019-03-18 02:01:00 +0000

    Can now be queried

--- flaskapp.py
+++ flaskapp.py
@@ -13,50 +13,38 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ubuntu@localhost:
 
 db = SQLAlchemy(app)
 
+# user_wordmaps = db.Table("user_wordmaps", db.Model.metadata,
+#                         db.Column('user_id', db.Integer, db.ForeignKey('Users.id')),
+#                         db.Column('words_id', db.String(128), db.ForeignKey('Maps.Words'))
+#                         # db.UniqueConstraint('user_id', 'words_id')
+#                         )
 
-user_wordmaps = db.Table("user_wordmaps", db.Model.metadata,
-                        db.Column('user_id', db.Integer, db.ForeignKey('Users.id')),
-                        db.Column('words_id', db.String(128), db.ForeignKey('Maps.Words'))
-                        # db.UniqueConstraint('user_id', 'words_id')
-                        )
-
-class User(db.Model):
-  __tablename__ = 'Users'
-  id = db.Column(db.Integer, primary_key=True)
+class User_Map(db.Model):
+  __tablename__ = 'User_Map'
+  id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
+  created_at = db.Column(db.DateTime, default=datetime.utcnow(), nullable=False)
+  user_id = db.Column(db.Integer, nullable=True)
   access_token = db.Column(db.String(2056), unique=True, nullable=False)
   name = db.Column(db.String(128), unique=True, nullable=False)
   email = db.Column(db.String(128), unique=True, nullable=False)
   img_link = db.Column(db.String(2056))
+  Address = db.Column(db.String(128), unique=True, nullable=True)
+  Words = db.Column(db.String(128), nullable=False, unique=True)
+  Latitude = db.Column(db.Integer, nullable=True, default=0)
+  Longitude = db.Column(db.Integer, nullable=True, default=0)
   # maps = db.relationship("Map", secondary=user_wordmaps)
   # Maps = db.relationship('Map', back_populates='Map', viewonly=False)
   def __repr__(self):
-    return 'Users({}, {}, {}, {}, {})'.format(self.id, self.access_token, self.name, self.email, self.img_link) 
+    return 'User_Map({}, {}, {}, {}, {}, {}, {}, {}, {})'.format(self.user_id, self.access_token, self.name, self.email, self.img_link, self.Address, self.Words, self.Latitude, self.Longitude) 
 
-class Map(db.Model):
-  __tablename__ = 'Maps'
-  id = db.Column(db.Integer, nullable=True)
-  Address = db.Column(db.String(128), unique=True, nullable=True)
-  Latitude = db.Column(db.Integer, nullable=True, default=0)
-  Longitude = db.Column(db.Integer, nullable=True, default=0)
-  # Name = db.Column(db.Text, nullable=True)
-  Words = db.Column(db.String(128), primary_key=True, nullable=False)
-  users_rel = db.relationship("Users", secondary=user_wordmaps)
-  # Words = db.Column(db.Unicode)
-  # Created_At = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-  # User = db.relationship('User', back_populates='User', viewonly=False)
-  # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-  def __repr__(self):
-    return 'Maps({}, {}, {}, {})'.format(self.Address, self.Latitude, self.Longitude, self.users)  
-    # return f"Maps('{self.Address}', '{self.Latitude}', '{self.Longitude}')"
+# class Map(db.Model):
+#   __tablename__ = 'Maps'
+#   user_id = db.Column(db.Integer, nullable=True)
+#   Address = db.Column(db.String(128), unique=True, nullable=True)
+#   Latitude = db.Column(db.Integer, nullable=True, default=0)
+#   Longitude = db.Column(db.Integer, nullable=True, default=0)
+#   Words = db.Column(db.String(128), primary_key=True, nullable=False, unique=True)
 
-# posts = [
-#   {
-#     'Address': 'San Francisco, CA 94128, United States',
-#     'Latitude': '37.6148', 
-#     'Longitude': '-122.39179', 
-#     'Name': 'San Francisco Int'l Airport', 
-#     'Words': 'remind.scare.nail'
-#     # 'Date_posted': 'April 20, 2018'
-#   }
-# ]
+#   def __repr__(self):
+#     return 'Maps({}, {}, {}, {}, {})'.format(self.user_id, self.Address, self.Latitude,self.Longitude, self.Words)
